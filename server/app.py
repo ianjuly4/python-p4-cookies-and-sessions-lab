d8508f13754ecd38e6b444a5e74a96033597468b
Ian July <ianjuly4@gmail.com>
2024-12-02 12:54:24 -0700

    cookies session lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,30 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
     pass
 
-@app.route('/articles/<int:id>')
+
+@app.route('/articles/<int:id>', methods=["GET"])
 def show_article(id):
+        response_dict = Article.query.filter_by(id=id).first().to_dict()
 
-    pass
+        session['page_views'] = session.get('page_views', 0) + 1
+
+        if session['page_views'] > 3:
+            response = make_response(
+                {'message': 'Maximum pageview limit reached'},
+                401
+            )
+            return response
+        else:
+            response = make_response(
+                response_dict,
+                200,
+            )
+
+            return response
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
